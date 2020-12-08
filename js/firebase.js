92d2d63b2e73ec65c352f872bdd6ae9abc71cc8b
majahoberg <eaamhob@students.eaaa.dk>
2020-12-08 14:18:18 +0100

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyDZnVlneuJHUs8AwH4FajXXViKvVsvTFQ4",
+    authDomain: "campingaarhus-73290.firebaseapp.com",
+    projectId: "campingaarhus-73290",
+    storageBucket: "campingaarhus-73290.appspot.com",
+    messagingSenderId: "251319843186",
+    appId: "1:251319843186:web:ed3d77423d223055bd85eb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
