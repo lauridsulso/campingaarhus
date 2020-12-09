b5b737bfa42dc96ae623ddd095ae46c1a2e43385
Tine-Dahlin <56149807+Tine-Dahlin@users.noreply.github.com>
2020-12-09 12:52:39 +0100

    .

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ function createPlayers() {
 }
 
 
-// exit knap i spillet (maja)
+// exit knap i spillet (maja, tine)
 
 function exit() {
     let page = (confirm("Er du sikker på, at du vil afslutte spillet?")); 
@@ -75,7 +75,6 @@ function createGame() {
         players: newPlayers
     };
     console.log(newGame);
-
 }
 
 
