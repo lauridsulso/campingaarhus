3417e57498f96e9c1c60f126d849ecd469539c04
pernillehm <56149661+pernillehm@users.noreply.github.com>
2020-12-10 11:07:07 +0100

    scoreboard popup

--- js/main.js
+++ js/main.js
@@ -91,3 +91,31 @@ function createPlayersHoles() {
 
 
 
+
+
+//POPUP boks til scoreboard
+// Get the modal
+var modal = document.getElementById("scoreboard-Modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("scoreboard-knap");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
